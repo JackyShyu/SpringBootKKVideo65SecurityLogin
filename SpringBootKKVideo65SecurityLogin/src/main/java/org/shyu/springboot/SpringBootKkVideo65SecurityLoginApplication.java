c6fe29e7fdca7b033a2f16337a34932c2c6b7126
jackyshyu <jackyshyu@gmail.com>
2018-09-29 11:44:39 -0500

    Add new files

---
+++ SpringBootKKVideo65SecurityLogin/src/main/java/org/shyu/springboot/SpringBootKkVideo65SecurityLoginApplication.java
@@ -0,0 +1,12 @@
+package org.shyu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKkVideo65SecurityLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKkVideo65SecurityLoginApplication.class, args);
+	}
+}
---
+++ SpringBootKKVideo65SecurityLogin/src/test/java/org/shyu/springboot/SpringBootKkVideo65SecurityLoginApplicationTests.java
@@ -0,0 +1,16 @@
+package org.shyu.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootKkVideo65SecurityLoginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
